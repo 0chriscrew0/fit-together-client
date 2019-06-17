f94983e79e869d012452b7e0e407c9d9379f8ef7
Christopher Crow <0chriscrew0@gmail.com>
2019-06-17 15:52:58 -0500

    add navlinks to header and style active state for links

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 
 import Logo from "../../img/FitTogether-logo-v2.png";
 import ProfilePic from "../../img/profile-pic.jpeg";
+import { NavLink } from "react-router-dom";
 
 export default class Header extends React.PureComponent {
   render() {
     return (
       <header className="header">
         <div className="container-fluid">
-          <nav className="navbar navbar-expand navbar-light bg-light px-0">
+          <nav className="navbar navbar-expand navbar-light bg-light px-0 pb-0">
             <div className="row w-100 mx-auto">
               <div className="col-4 col-md-2 logo-wrapper">
                 <a className="navbar-brand" href="#">
@@ -27,28 +28,28 @@ export default class Header extends React.PureComponent {
                 >
                   <ul className="navbar-nav">
                     <li className="nav-item">
-                      <a className="nav-link" href="#">
+                      <NavLink className="nav-link" to="/feed">
                         <i className="fas fa-home" />{" "}
                         <span className="header-link">Home</span>
-                      </a>
+                      </NavLink>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">
+                      <NavLink className="nav-link" to="/workouts">
                         <i className="fas fa-dumbbell" />{" "}
                         <span className="header-link">Workouts</span>
-                      </a>
+                      </NavLink>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">
+                      <NavLink className="nav-link" to="/routines">
                         <i className="fas fa-clock" />{" "}
                         <span className="header-link">Routines</span>
-                      </a>
+                      </NavLink>
                     </li>
                     <li className="nav-item">
-                      <a className="nav-link" href="#">
+                      <NavLink className="nav-link" to="/meal-plans">
                         <i className="fas fa-utensils" />{" "}
                         <span className="header-link">Meal Plans</span>
-                      </a>
+                      </NavLink>
                     </li>
                   </ul>
                 </div>
