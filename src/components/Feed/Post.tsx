f535274921b7b199b8d95a704cbdef99065c602f
Christopher Crow <0chriscrew0@gmail.com>
2019-06-22 11:39:59 -0500

    add Post component to feed and layout content

---
+++ src/components/Feed/Post.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+import ProfilePicture from "../../img/profile-pic.jpeg";
+
+export default class Post extends React.PureComponent {
+  render() {
+    return (
+      <div className="feed-post">
+        <div className="header">
+          <div className="user">
+            <img
+              className="img-fluid"
+              src={ProfilePicture}
+              alt="User Profile"
+            />
+            <h6>Username</h6>
+          </div>
+          <div className="feedback">
+            <span className="likes">
+              123 <i className="fas fa-like" />
+            </span>
+            <span className="comments">
+              8 <i className="fas fa-comment" />
+            </span>
+          </div>
+        </div>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+            soluta inventore quia obcaecati at, modi blanditiis accusamus
+            quaerat fugit non!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import Post from "./Post";
 
 export default class Feed extends React.PureComponent {
   render() {
-    return <div className="feed" />;
+    return (
+      <div className="feed">
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    );
   }
 }
