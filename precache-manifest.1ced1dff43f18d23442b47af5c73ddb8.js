2db5c5f8876e82eca91a11e41cc5cd379f5d1033
Christopher Crow <0chriscrew0@gmail.com>
2019-06-22 20:50:31 -0500

    Updates

--- precache-manifest.1ced1dff43f18d23442b47af5c73ddb8.js
+++ precache-manifest.1ced1dff43f18d23442b47af5c73ddb8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c473e50b3fdde1e9ae6df8c4c79aca25",
+    "revision": "cadef04958be0f319526c921cb676f73",
     "url": "/fit-together/index.html"
   },
   {
-    "revision": "184baf587e25802049e1",
-    "url": "/fit-together/static/css/main.4a03fa43.chunk.css"
+    "revision": "c5e3b20867b0818f7d89",
+    "url": "/fit-together/static/css/main.6504b88a.chunk.css"
   },
   {
-    "revision": "b3352b2dbff5c3733320",
-    "url": "/fit-together/static/js/2.002de6f5.chunk.js"
+    "revision": "ea05fbf4fcbfd5f42974",
+    "url": "/fit-together/static/js/2.596fbca1.chunk.js"
   },
   {
-    "revision": "184baf587e25802049e1",
-    "url": "/fit-together/static/js/main.ad1164a4.chunk.js"
+    "revision": "c5e3b20867b0818f7d89",
+    "url": "/fit-together/static/js/main.6002c104.chunk.js"
   },
   {
     "revision": "a4e75f8284930621b099",
