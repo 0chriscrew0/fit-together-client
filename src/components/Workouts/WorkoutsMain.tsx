ae6ef96248889c03047be7464e755744d10792aa
Christopher Crow <0chriscrew0@gmail.com>
2019-07-22 20:07:36 -0500

    add more dummy data

--- src/components/Workouts/WorkoutsMain.tsx
+++ src/components/Workouts/WorkoutsMain.tsx
@@ -11,23 +11,110 @@ moment.relativeTimeThreshold("h", 24);
 moment.relativeTimeThreshold("d", 31);
 moment.relativeTimeThreshold("M", 12);
 
-const dummyData = {
-  title: "Best Workout",
-  creator: {
-    username: "Brad"
+const dummyData = [
+  {
+    title: "Best Workout",
+    creator: {
+      username: "Brad"
+    },
+    date: moment("2019072219:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 2,
+    comments: 4,
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse debitis unde sed saepe assumenda maxime ut fugit soluta blanditiis harum?",
+    duration: {
+      hours: 1,
+      minutes: 30
+    },
+    rating: 4,
+    intensity: 7
   },
-  date: moment("2017011909:48:54", "YYYYMMDDHH:mm:ss").format(),
-  saves: 2,
-  comments: 4,
-  description:
-    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse debitis unde sed saepe assumenda maxime ut fugit soluta blanditiis harum?",
-  duration: {
-    hours: 1,
-    minutes: 30
+  {
+    title: "Insane Workout",
+    creator: {
+      username: "Chris"
+    },
+    date: moment("2019072209:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 54,
+    comments: 30,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, quaerat! Fugit perspiciatis assumenda temporibus aliquid autem cumque eveniet. Eos tempora aliquam vitae autem, delectus unde facere temporibus eius perferendis commodi ex quibusdam maxime dolorem cupiditate eligendi eum provident laudantium quidem adipisci reprehenderit repudiandae quae. Minus, eum facere. Dolorem veniam commodi ab aperiam ad, consequuntur suscipit illo sequi fuga molestias dolorum.",
+    duration: {
+      hours: 0,
+      minutes: 45
+    },
+    rating: 5,
+    intensity: 9.4
   },
-  rating: 4,
-  intensity: 7
-};
+  {
+    title: "Easy Warmup",
+    creator: {
+      username: "Chad"
+    },
+    date: moment("2019042009:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 13,
+    comments: 1,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque saepe quo quia debitis amet est consequuntur asperiores ut, repudiandae ullam. Sint ratione cum corrupti accusantium dicta voluptatem delectus, fugit eveniet fugiat exercitationem? Est, placeat fuga. Laborum ipsam sit cupiditate eaque.",
+    duration: {
+      hours: 0,
+      minutes: 10
+    },
+    rating: 4.7,
+    intensity: 2
+  },
+  {
+    title: "Great Cooldown",
+    creator: {
+      username: "Brad"
+    },
+    date: moment("2018011309:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 4,
+    comments: 0,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aspernatur ipsam omnis?",
+    duration: {
+      hours: 0,
+      minutes: 15
+    },
+    rating: 4.2,
+    intensity: 4
+  },
+  {
+    title: "Christmas Workout (Intense)",
+    creator: {
+      username: "Chris"
+    },
+    date: moment("2018122509:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 679,
+    comments: 102,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non assumenda quibusdam iusto doloribus aperiam?",
+    duration: {
+      hours: 1,
+      minutes: 15
+    },
+    rating: 4.8,
+    intensity: 10
+  },
+  {
+    title: "My first workout!",
+    creator: {
+      username: "Brad"
+    },
+    date: moment("2017041910:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 198,
+    comments: 54,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos accusantium corrupti aliquid ipsum aspernatur. Minus voluptate magnam excepturi. Nihil, odio magnam, aliquid quod sunt fugiat assumenda cumque repudiandae eligendi eum id nobis labore. Nesciunt minima veniam quis cum. Molestiae, culpa.",
+    duration: {
+      hours: 1,
+      minutes: 0
+    },
+    rating: 1.2,
+    intensity: 3
+  }
+];
 
 const WorkoutsMain: React.FunctionComponent = () => (
   <div className="workouts-main">
@@ -40,13 +127,12 @@ const WorkoutsMain: React.FunctionComponent = () => (
       </button>
     </div>
 
-    <WorkoutCard {...dummyData} />
-    <WorkoutCard {...dummyData} />
-    <WorkoutCard {...dummyData} />
-
-    <WorkoutCard {...dummyData} />
-    <WorkoutCard {...dummyData} />
-    <WorkoutCard {...dummyData} />
+    <WorkoutCard {...dummyData[0]} />
+    <WorkoutCard {...dummyData[1]} />
+    <WorkoutCard {...dummyData[2]} />
+    <WorkoutCard {...dummyData[3]} />
+    <WorkoutCard {...dummyData[4]} />
+    <WorkoutCard {...dummyData[5]} />
   </div>
 );
 
