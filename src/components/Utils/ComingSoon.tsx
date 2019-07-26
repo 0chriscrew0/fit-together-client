af9bef5326846e07c841fbecf786e13c0714633a
Christopher Crow <0chriscrew0@gmail.com>
2019-07-26 14:31:56 -0500

    create ComingSoon component

---
+++ src/components/Utils/ComingSoon.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+interface Props {
+  feature: String;
+  description: String;
+}
+
+export default class ComingSoon extends React.PureComponent<Props> {
+  render() {
+    return (
+      <div className="coming-soon">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 mx-auto">
+              <h2>Coming Soon</h2>
+              <h5>{this.props.feature}</h5>
+              <p>{this.props.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
