4358d9d931d96dc57bce4f967b12b27349d98526
Christopher Crow <0chriscrew0@gmail.com>
2019-08-01 16:49:47 -0500

    make WorkoutFilter column wider on smaller screens

--- src/components/Workouts/index.tsx
+++ src/components/Workouts/index.tsx
@@ -9,10 +9,10 @@ export default class Workouts extends React.PureComponent {
       <div className="workouts">
         <div className="container">
           <div className="row">
-            <div className="col-md-8 px-0">
+            <div className="col-md-6 col-lg-7 col-xl-8 px-0">
               <WorkoutsMain />
             </div>
-            <div className="col-md-4 px-0">
+            <div className="col-md-6 col-lg-5 col-xl-4 px-0">
               <WorkoutsFilter />
             </div>
           </div>
