f58b30cba78220bd73545a27f72dd92b4c1b9b3c
Christopher Crow <0chriscrew0@gmail.com>
2019-08-01 17:34:23 -0500

    add content and styling to Routines page

---
+++ src/components/Routines/RoutinesFilter.tsx
@@ -0,0 +1,122 @@
+import * as React from "react";
+
+const RoutinesFilter: React.FunctionComponent = () => (
+  <div className="routines-filter">
+    <div className="filter-header">
+      <h5>Filter</h5>
+      <button className="btn btn-sm btn-primary">Apply</button>
+    </div>
+    <div className="filters">
+      <div className="rating pt-4">
+        <h6>Rating</h6>
+        <div className="form-group">
+          <select
+            className="form-control form-control-sm"
+            name="rating"
+            id="rating"
+          >
+            <option value="0">less than</option>
+            <option value="1">greater than</option>
+          </select>
+        </div>
+        <div className="star-buttons">
+          <button className="btn btn-sm btn-outline-primary border-0">
+            <i className="fas fa-star" />
+          </button>
+          <button className="btn btn-sm btn-outline-primary border-0">
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+          </button>
+          <button className="btn btn-sm btn-outline-primary border-0">
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+          </button>
+          <button className="btn btn-sm btn-outline-primary border-0">
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+          </button>
+          <button className="btn btn-sm btn-outline-primary border-0">
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+            <i className="fas fa-star" />
+          </button>
+        </div>
+      </div>
+
+      <div className="days py-4">
+        <h6>Days per week</h6>
+        <div className="form-group">
+          <select
+            className="form-control form-control-sm"
+            name="days"
+            id="days"
+          >
+            <option value="0">less than</option>
+            <option value="1">more than</option>
+          </select>
+        </div>
+        <div className="days-buttons">
+          <button className="btn btn-sm btn-outline-primary">1</button>
+          <button className="btn btn-sm btn-outline-primary">2</button>
+          <button className="btn btn-sm btn-outline-primary">3</button>
+          <button className="btn btn-sm btn-outline-primary">4</button>
+          <button className="btn btn-sm btn-outline-primary">5</button>
+          <button className="btn btn-sm btn-outline-primary">6</button>
+          <button className="btn btn-sm btn-outline-primary">7</button>
+        </div>
+      </div>
+      <div className="intensity pb-4">
+        <h6>Intensity</h6>
+        <div className="form-row">
+          <div className="col form-group mb-0">
+            <label htmlFor="min">Min</label>
+            <select
+              className="form-control form-control-sm"
+              name="min"
+              id="min"
+              defaultValue="1"
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <div className="col form-group mb-0">
+            <label htmlFor="max">Max</label>
+            <select
+              className="form-control form-control-sm"
+              name="max"
+              id="max"
+              defaultValue="10"
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default RoutinesFilter;
--- src/components/Routines/index.tsx
+++ src/components/Routines/index.tsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 
 import RoutinesMain from "./RoutinesMain";
-import RoutinesSide from "./RoutinesSide";
-import ComingSoon from "../Utils/ComingSoon";
+import RoutinesFilter from "./RoutinesFilter";
 
 export default class Routines extends React.PureComponent {
   render() {
     return (
-      <div className="workouts">
+      <div className="routines">
         <div className="container">
           <div className="row">
             <div className="col-md-8 px-0">
               <RoutinesMain />
             </div>
             <div className="col-md-4 px-0">
-              <RoutinesSide />
+              <RoutinesFilter />
             </div>
           </div>
         </div>
