be80756e97c0f0c22305d58597ecbee53f25d34a
Christopher Crow <0chriscrew0@gmail.com>
2019-08-01 19:47:57 -0500

    Updates

--- precache-manifest.2ab6b2590b430c60c0e60234581a0e82.js
+++ precache-manifest.2ab6b2590b430c60c0e60234581a0e82.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88f0c0a6b52da400f57496ce54e34971",
+    "revision": "ea656a21bc7a1539ac87f0eaad9c3480",
     "url": "/fit-together/index.html"
   },
   {
-    "revision": "0dd6ac7552e8ce7dba2b",
-    "url": "/fit-together/static/css/main.73cae656.chunk.css"
+    "revision": "57b53000ff5c1b2bdf5d",
+    "url": "/fit-together/static/css/main.afd94819.chunk.css"
   },
   {
     "revision": "fb7a4bd9a8e49c755fab",
     "url": "/fit-together/static/js/2.f70d6b9e.chunk.js"
   },
   {
-    "revision": "0dd6ac7552e8ce7dba2b",
-    "url": "/fit-together/static/js/main.3aed6f4c.chunk.js"
+    "revision": "57b53000ff5c1b2bdf5d",
+    "url": "/fit-together/static/js/main.c3c94072.chunk.js"
   },
   {
     "revision": "a4e75f8284930621b099",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fit-together/precache-manifest.709764ba6dbdd9a2ee6a92fbbea31076.js"
+  "/fit-together/precache-manifest.2ab6b2590b430c60c0e60234581a0e82.js"
 );
 
 self.addEventListener('message', (event) => {
