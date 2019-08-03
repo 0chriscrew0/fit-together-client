4821a0e81c787a21485f4e487b9caa5dec77ecb6
Christopher Crow <0chriscrew0@gmail.com>
2019-08-03 14:13:15 -0500

    add dummy data for meal plans

--- src/components/MealPlans/MealPlansMain.tsx
+++ src/components/MealPlans/MealPlansMain.tsx
@@ -13,88 +13,86 @@ moment.relativeTimeThreshold("M", 12);
 
 const dummyData = [
   {
-    title: "Best Workout",
-    creator: {
-      username: "Brad"
-    },
-    date: moment("2019072219:48:54", "YYYYMMDDHH:mm:ss").format(),
-    saves: 2,
-    comments: 4,
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse debitis unde sed saepe assumenda maxime ut fugit soluta blanditiis harum?",
-    days: 5,
-    rating: 4,
-    price: 2
-  },
-  {
-    title: "Insane Workout",
+    title: "Best Meal Plan",
     creator: {
       username: "Chris"
     },
-    date: moment("2019072209:48:54", "YYYYMMDDHH:mm:ss").format(),
-    saves: 54,
-    comments: 30,
+    date: moment("2019080119:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 453,
+    comments: 142,
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+    days: 3,
+    rating: 4.9,
+    price: 1
+  },
+  {
+    title: "Vegan Meals!!",
+    creator: {
+      username: "Chad"
+    },
+    date: moment("2019073109:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 32,
+    comments: 17,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, quaerat! Fugit perspiciatis assumenda temporibus aliquid autem cumque eveniet. Eos tempora aliquam vitae autem, delectus unde facere temporibus eius perferendis commodi ex quibusdam maxime dolorem cupiditate eligendi eum provident laudantium quidem adipisci reprehenderit repudiandae quae. Minus, eum facere. Dolorem veniam commodi ab aperiam ad, consequuntur suscipit illo sequi fuga molestias dolorum.",
-    days: 5,
+      "Soluta, quaerat! Fugit perspiciatis assumenda temporibus aliquid autem cumque eveniet. Eos tempora aliquam vitae autem, delectus unde facere temporibus eius perferendis commodi ex quibusdam maxime dolorem cupiditate eligendi eum provident laudantium quidem adipisci reprehenderit repudiandae quae. Minus, eum facere. Dolorem veniam commodi ab aperiam ad, consequuntur suscipit illo sequi fuga molestias dolorum.",
+    days: 7,
+    rating: 4.2,
+    price: 4
+  },
+  {
+    title: "Cheap Plan",
+    creator: {
+      username: "Brad"
+    },
+    date: moment("2019042009:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 22,
+    comments: 5,
+    description:
+      "Atque saepe quo quia debitis amet est consequuntur asperiores ut. Sint ratione cum corrupti accusantium dicta voluptatem delectus, fugit eveniet fugiat exercitationem? Est, placeat fuga. Laborum ipsam sit cupiditate eaque.",
+    days: 6,
+    rating: 3,
+    price: 1
+  },
+  {
+    title: "Great Meals",
+    creator: {
+      username: "Chris"
+    },
+    date: moment("2017011309:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 44,
+    comments: 21,
+    description: "Alias aspernatur ipsam omnis?",
+    days: 4,
     rating: 5,
     price: 3
   },
   {
-    title: "Easy Warmup",
+    title: "Christmas Diet!",
     creator: {
       username: "Chad"
     },
-    date: moment("2019042009:48:54", "YYYYMMDDHH:mm:ss").format(),
-    saves: 13,
-    comments: 1,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque saepe quo quia debitis amet est consequuntur asperiores ut, repudiandae ullam. Sint ratione cum corrupti accusantium dicta voluptatem delectus, fugit eveniet fugiat exercitationem? Est, placeat fuga. Laborum ipsam sit cupiditate eaque.",
-    days: 5,
-    rating: 4.7,
-    price: 2
-  },
-  {
-    title: "Great Cooldown",
-    creator: {
-      username: "Brad"
-    },
-    date: moment("2018011309:48:54", "YYYYMMDDHH:mm:ss").format(),
-    saves: 4,
-    comments: 0,
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aspernatur ipsam omnis?",
-    days: 5,
-    rating: 4.2,
-    price: 2
-  },
-  {
-    title: "Christmas Workout (Intense)",
-    creator: {
-      username: "Chris"
-    },
-    date: moment("2018122509:48:54", "YYYYMMDDHH:mm:ss").format(),
+    date: moment("2015122509:48:54", "YYYYMMDDHH:mm:ss").format(),
     saves: 679,
     comments: 102,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non assumenda quibusdam iusto doloribus aperiam?",
-    days: 5,
+      "Consectetur adipisicing elit. Non assumenda quibusdam iusto doloribus aperiam?",
+    days: 1,
     rating: 4.8,
-    price: 4
+    price: 3
   },
   {
-    title: "My first workout!",
+    title: "My first meal plan!",
     creator: {
       username: "Brad"
     },
-    date: moment("2017041910:48:54", "YYYYMMDDHH:mm:ss").format(),
-    saves: 198,
-    comments: 54,
+    date: moment("1994041910:48:54", "YYYYMMDDHH:mm:ss").format(),
+    saves: 1,
+    comments: 0,
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos accusantium corrupti aliquid ipsum aspernatur. Minus voluptate magnam excepturi. Nihil, odio magnam, aliquid quod sunt fugiat assumenda cumque repudiandae eligendi eum id nobis labore. Nesciunt minima veniam quis cum. Molestiae, culpa.",
+      "Quos accusantium corrupti aliquid ipsum aspernatur. Minus voluptate magnam excepturi. Nihil, odio magnam, aliquid quod sunt fugiat assumenda cumque repudiandae eligendi eum id nobis labore. Nesciunt minima veniam quis cum. Molestiae, culpa.",
     days: 5,
-    rating: 1.2,
-    price: 3
+    rating: 1,
+    price: 4
   }
 ];
 
