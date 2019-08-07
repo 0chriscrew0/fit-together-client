0fba51d3660eb7292a10f1b00d281829044dcab4
Christopher Crow <0chriscrew0@gmail.com>
2019-08-07 16:48:13 -0500

    add meal plans filter as modal on smaller devices

--- src/components/MealPlans/MealPlansMain.tsx
+++ src/components/MealPlans/MealPlansMain.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import moment from "moment";
 
 import MealPlanCard from "./MealPlanCard";
+import MealPlansFilter from "./MealPlansFilter";
 
 moment.relativeTimeRounding(Math.floor);
 
@@ -100,11 +101,61 @@ const MealPlansMain: React.FunctionComponent = () => (
   <div className="meal-plans-main">
     <div className="meal-plans-main-header">
       <h5>Meal Plans</h5>
-      <button className="btn btn-sm btn-primary">
-        New Meal Plan
-        <i className="fas fa-utensils pl-2" />
-        <i className="fas fa-plus plus-icon" />
-      </button>
+      <div>
+        <button
+          className="btn border-0 mr-2 p-1 d-lg-none"
+          data-toggle="modal"
+          data-target="#filterModal"
+        >
+          <i className="fas fa-sliders-h text-primary" />
+        </button>
+        <div
+          className="modal fade"
+          id="filterModal"
+          tabIndex={-1}
+          role="dialog"
+          aria-labelledby="filterModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="filterModalLabel">
+                  Filters
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <MealPlansFilter />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Cancel
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Apply
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button className="btn btn-sm btn-primary">
+          New Meal Plan
+          <i className="fas fa-utensils pl-2" />
+          <i className="fas fa-plus plus-icon" />
+        </button>
+      </div>
     </div>
 
     <MealPlanCard {...dummyData[0]} />
--- src/components/MealPlans/index.tsx
+++ src/components/MealPlans/index.tsx
@@ -9,10 +9,10 @@ export default class MealPlans extends React.PureComponent {
       <div className="meal-plans">
         <div className="container">
           <div className="row">
-            <div className="col-md-6 col-lg-7 col-xl-8 px-0">
+            <div className="col-md-12 col-lg-7 col-xl-8 px-0">
               <MealPlansMain />
             </div>
-            <div className="col-md-6 col-lg-5 col-xl-4 px-0">
+            <div className="d-none d-lg-block col-lg-5 col-xl-4 px-0">
               <MealPlansFilter />
             </div>
           </div>
